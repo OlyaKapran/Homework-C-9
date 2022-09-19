5d64da16703ebdd6feb1798433092ea3743a6fee
Olga <olyakapran@yandex.by>
2022-09-19 15:09:04 +0300

    Second commit

---
+++ задача66/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace gmvkr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void GapNumberSum (int M, int N, int sum)
+            {
+                if (M > N) 
+                {
+                    Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+                    return;
+                }
+                sum = sum + (M++);
+                GapNumberSum(M, N, sum);
+            }
+
+            GapNumberSum(5, 15, 0);
+        }
+    }
+}
