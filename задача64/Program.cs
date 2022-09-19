7e8ca3c5e90a29541b3558e4cd04f243caa5bbe4
Olga <olyakapran@yandex.by>
2022-09-19 15:07:49 +0300

    first commit

---
+++ задача64/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace задача64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sum(int N, int M)
+            {
+                if (N == M) return $"{M} ";
+                return Sum(N - 1, M) + N + " ";
+            }
+            Console.WriteLine(Sum(15, 5));
+        }
+    }
+}
