fad813578807d5307e0ef225760b7590219beee8
Olga <olyakapran@yandex.by>
2022-09-19 15:09:43 +0300

    Third commit

---
+++ задача68/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace задача68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Akkerman(int m, int n)
+            {
+                if (m == 0) return n + 1;
+                else if (n == 0 && m > 0) return Akkerman(m - 1, 1);
+                else return (Akkerman(m - 1, Akkerman(m, n - 1)));
+
+            }
+            Console.WriteLine(Akkerman(3, 2));
+        }
+    }
+}
